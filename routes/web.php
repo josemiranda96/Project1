c66db1a0ffc4fee63dc64c487e14ab0e1169a2fe
vagrant <vagrant@homestead>
2020-04-01 06:19:01 +0000

    Se agrego autentificacion y algunas rutas para la galeria de fotos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('fotos/{numero?}',function($numero = 'Sin Numero'){
+    return 'Estas en la galeria de fotos: '.$numero;    
+})->where('numero','[0-9]+');
+
+Route::view('galeria','fotos',['numero' =>125]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
